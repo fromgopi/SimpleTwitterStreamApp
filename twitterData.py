d066e6762ccfb40284dc3fdb436a28232b9340e2
Gopi Krishna Muktevi <muktevigk@handbookinc.com>
2018-04-27 10:31:30 +0530

    Modified Some logic

--- twitterData.py
+++ twitterData.py
@@ -1,32 +1,32 @@
 import tweepy
-import _pickle as pickle
+import codecs
 from credentials import *
 
 
 def tweet_config():
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-    api = tweepy.API(auth)
+    try:
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+        api = tweepy.API(auth)
 
-    return api
+        return api
+    except:
+        pass
 
 
 def extract_tweets(screen_name):
-    extractor = tweet_config()
-    tweets = extractor.user_timeline(screen_name=screen_name, count=1000, tweet_mode="extended")
-    new_tweet = [[tweet.full_text] for tweet in tweets]
-    #print(new_tweet)
-    return new_tweet
 
+    try:
+        extractor = tweet_config()
+        tweets = extractor.user_timeline(screen_name=screen_name, count=100, tweet_mode="extended")
+        with codecs.open('tweets/%s_tweets.csv' % screen_name, "w", "utf-8") as fp:
+            for items in tweets:
+                fp.write("\n")
+                fp.write(items.full_text)
+    except:
+        pass
 
-def writeToFile():
-    tweets = extract_tweets("realDonaldTrump")
-
-    with open("tweets.txt", "wb") as line:
-        for items in tweets:
-            for item in items:
-                line.write(u''.join(item + "\n").encode('utf-8').strip())
 
 
 if __name__ == '__main__':
-    writeToFile()
+    extract_tweets("realDonaldTrump")
